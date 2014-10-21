762e8290df5f4fe5f8e3dcba5d8c1c323e7d78b3
Jean Kim <jeankim29@gmail.com>
2014-10-21 19:13:14 -0400

    Completed namespace assigment

--- js/namespace.js
+++ js/namespace.js
@@ -1,3 +1,20 @@
 function namespace(path){
-  // YOUR CODE HERE
+	//split path on period
+ 	var splitPath = path.split('.');
+
+	window[splitPath[0]] = window[splitPath[0]] || {};
+
+	var globalOb = window[splitPath[0]];
+
+	for (var i = 1; i < splitPath.length; i++) {
+		var nestedOb= {};
+
+		if (globalOb[splitPath[i]] === undefined) {
+			globalOb[splitPath[i]] = nestedOb;
+		} 
+
+		globalOb = globalOb[splitPath[i]];
+		
+	}
+
 }
