c31034bf88bd06fcadc255aa8b90d51dd63107ad
Price <205605@carolyn-price-1046.nyhq.nytint.com>
2014-10-22 16:18:30 -0400

    ccprice nytimes assignment

--- js/namespace.js
+++ js/namespace.js
@@ -1,3 +1,14 @@
 function namespace(path){
-  // YOUR CODE HERE
+    var names;
+    var namespace = path.split('.');
+    if(window[namespace[0]] === undefined) {
+        window[namespace[0]] = {};
+    }
+    var names = window[namespace[0]];
+    for(var i = 1; i < namespace.length; i++) {
+        if(names[namespace[i]] === undefined){
+            names[namespace[i]] = {};
+        }
+        names = names[namespace[i]];
+    }
 }
