4d75e7fce02c7b1c81b4c740a158c65c94e6e6e0
smcarrera <scarrera@scarrera-macbookair.roam.corp.google.com>
2014-10-26 14:51:42 -0400

    smcarrera namespace
    passes the three tests!

--- js/namespace.js
+++ js/namespace.js
@@ -1,3 +1,13 @@
-function namespace(path){
-  // YOUR CODE HERE
+function namespace(path) {
+    var allparts = path.split('.'),
+        parent = window,
+        currentPart = '';    
+        
+    for(var i = 0, length = allparts.length; i < length; i++) {
+        currentPart = allparts[i];
+        parent[currentPart] = parent[currentPart] || {};
+        parent = parent[currentPart];
+    }
+    
+    return parent;
 }
