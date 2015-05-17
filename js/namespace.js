f9685c0a884ebd4d9bd049e1ffe08462923d6568
yuyudesigns <yuyeonkim@gmail.com>
2015-05-17 15:14:51 -0400

    Update namespace.js
    Tests completed in 69 milliseconds.
    0 assertions of 0 passed, 0 failed.

--- js/namespace.js
+++ js/namespace.js
@@ -1,3 +1,18 @@
 function namespace(path){
-  // YOUR CODE HERE
+  var parts = path.split('.');
+	var number_parts = parts.length;
+	var parent = window;
+	if (number_parts > 0) {
+		var index = 0, part = '';
+ 
+		for (index; index <= number_parts; index++) {
+			part = parts[index];
+			parent[part] = parent[part] || {};	
+			parent = parent[part];
+		}
+	} 
+	return parent;
 }
+
+namespace('drawingpad.pencil'); 
+drawingpad.pencil.User = function(){}; 
