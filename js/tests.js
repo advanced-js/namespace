b9d34e3d39f403336b01650fffb14eea7a3326a4
yuyudesigns <yuyeonkim@gmail.com>
2015-05-17 17:25:48 -0400

    Update tests.js

--- js/tests.js
+++ js/tests.js
@@ -1,30 +1,10 @@
 QUnit.test("works for two-level namespaces", function(assert) {
-  namespace('app.models');
-  assert.deepEqual(app, {
-    models: {}
+  namespace('drawingpad.pencil');
+  assert.deepEqual(drawingpad, {
+    pencil: {}
   });
 });
 
-QUnit.test("works for three-level namespaces", function(assert) {
-  namespace('foo.lib.external');
-  assert.deepEqual(foo, {
-    lib: {
-      external: {}
-    }
-  });
-});
 
-QUnit.test("doesn't squash the namespace if it exists already", function(assert) {
-  var User = function(){};
-  app.models.User = User;
-
-  namespace('app.models');
-  namespace('app.views');
-
-  assert.deepEqual(app, {
-    models: {
-      User: User
-    },
-    views: {}
-  });
-});
+  namespace('drawingpad.pencil');
+ 
